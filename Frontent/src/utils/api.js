2f60beb9660d91324315f5afec8d74ab45bce26f
Shivam9997 <shivamrathore2805@gmail.com>
2026-04-30 23:52:03 +0530

    Add new lines

--- Frontent/src/utils/api.js
+++ Frontent/src/utils/api.js
@@ -8,4 +8,4 @@ export const getAuthHeaders = () => {
 };
 
 // Base API URL
-export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
